a077b655b8e53715e54c558c96e2bbb4b38807f8
SalsaGal <fielddashos@gmail.com>
2022-12-14 00:37:20 +1100

    Add Renderer

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use rhachis::GameData;
+
+pub struct Renderer {
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Renderer {
+    pub fn new(data: &GameData) -> Self {
+        Self {}
     }
 }
+
+impl rhachis::graphics::Renderer for Renderer {}
